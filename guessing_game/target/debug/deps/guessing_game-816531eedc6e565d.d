74c85ef98b51941da7054a49ce7b9fee054d2fd0
Ethan Rogers <erogers6264@gmail.com>
2018-06-10 15:58:08 -0700

    Remove target from vc

--- guessing_game/target/debug/deps/guessing_game-816531eedc6e565d.d
+++
@@ -1,5 +0,0 @@
-/home/ethan/Downloads/coding/cautious-waddle/guessing_game/target/debug/deps/guessing_game-816531eedc6e565d: src/main.rs
-
-/home/ethan/Downloads/coding/cautious-waddle/guessing_game/target/debug/deps/guessing_game-816531eedc6e565d.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/ethan/Downloads/coding/cautious-waddle/guessing_game/target/debug/guessing_game: /home/ethan/Downloads/coding/cautious-waddle/guessing_game/src/main.rs
